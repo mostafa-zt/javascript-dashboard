43cc81fe7c3936276fb58c67a0f7606363438629
mostafa <you@example.com>
2021-02-28 12:14:10 -0500

    chnages

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
